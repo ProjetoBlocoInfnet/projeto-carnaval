791f098c424e732655e53a8b3905de6fff62e39b
TheFakeMontyOnTheRun <danielmonteiro@id.uff.br>
2015-04-23 23:13:04 -0300

    Adiciona os nomes bonitos para os grupos de escolas
    E defini o grupo especial para o Salgueiro do Mock

--- ProjetoCarnaval_Exemplo01/src/enumerator/Grupos.java
+++ ProjetoCarnaval_Exemplo01/src/enumerator/Grupos.java
@@ -2,5 +2,16 @@ package enumerator;
 
 public enum Grupos
 {
-	GrupoEspecial, SerieA, GrupoB, GrupoC, GrupoD, GrupoAvaliacao;
+	GrupoEspecial( "Grupo Especial"), 
+	SerieA( "Serie A"), 
+	GrupoB( "Grupo B"), 
+	GrupoC( "Grupo C"), 
+	GrupoD( "Grupo D"), 
+	GrupoAvaliacao( "Grupo Avaliação" );
+	
+	public final String nomeBonito;
+	
+	Grupos( String nomeBonito ) {
+		this.nomeBonito = nomeBonito;
+	}
 }
