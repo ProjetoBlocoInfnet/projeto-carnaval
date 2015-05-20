106662560928da0ff20626505a7db6a74e381562
Emmanuel Aranha <emmanuelaranha@gmail.com>
2015-05-19 23:16:21 -0300

    Ajuste na classe de conexão

--- ProjetoCarnaval_Exemplo01/src/dao/Conexao.java
+++ ProjetoCarnaval_Exemplo01/src/dao/Conexao.java
@@ -11,21 +11,32 @@ public class Conexao
 	
 	public static Connection getConexao(){
 		Connection c = null;
-		
+		/*
 		 String url = "jdbc:mysql://localhost/projeto_carnaval";  
 	     String login = "root";  
 	     String senha = "Debora0103";  
-			
+			*/
 		try {
 			try {
 				Class.forName("com.mysql.jdbc.Driver");
+				
+				return DriverManager.getConnection("jdbc:mysql://localhost/projeto_carnaval","root","93956619");
 			} catch (ClassNotFoundException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}  
-            return DriverManager.getConnection(url, login, senha);   
+            //return DriverManager.getConnection(url, login, senha);
+			//return DriverManager.getConnection("jdbc:mysql://localhost/projeto_carnaval","root","93956619");
 			
 			//c = DriverManager.getConnection("jdbc:mysql://localhost/projeto_carnaval","root","Debora0103");
+			//Ambiente dev Emmanuel
+			//c = DriverManager.getConnection("jdbc:mysql://localhost/projeto_carnaval","root","93956619");
+			//Ambiente dev Daniel
+			//c = DriverManager.getConnection("jdbc:mysql://localhost/projeto_carnaval","root","root");
+			//Ambiente dev Elom
+			//c = DriverManager.getConnection("jdbc:mysql://localhost/projeto_carnaval","root","root");
+			//Ambiente Produção
+			//c = DriverManager.getConnection("jdbc:mysql://localhost/projeto_carnaval","root","root");
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
