37df155ae52fc40f5e6e5843eafd845f9618611c
Emmanuel Aranha <emmanuelaranha@gmail.com>
2015-05-21 23:49:07 -0300

    Primeiros ajustes DesfileDAO

--- ProjetoCarnaval_Exemplo01/src/dao/DesfileDAO.java
+++ ProjetoCarnaval_Exemplo01/src/dao/DesfileDAO.java
@@ -1,5 +1,9 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
@@ -13,8 +17,13 @@ import negocio.Desfile;
 import negocio.Entidade;
 import negocio.EscolaSamba;
 import negocio.Jurado;
+import negocio.Quesito;
+import negocio.Pessoa.Sexos;
 
 public class DesfileDAO extends AbstractDAO implements DAO {
+	
+	private PreparedStatement pstmt;
+	private ResultSet rs;
 
 	private static Map<Integer,Desfile> desfiles = new HashMap<>();
 	
@@ -80,17 +89,40 @@ public class DesfileDAO extends AbstractDAO implements DAO {
 	@Override
 	public List<Entidade> obterTodos() {
 		List<Entidade> desfiles = new ArrayList<>();
-		for(int i=0; i< DesfileDAO.desfiles.size(); i++ )
-		{
-			desfiles.add(DesfileDAO.desfiles.get(i));
+		Connection c = getConnection();
+		String sql = "select * from carnaval join (carnaval_quesitos, carnaval_quesito_jurado, carnaval_posicao_jurado, carnaval_desfile_escola) on (carnaval.id_carnaval = carnaval_quesitos.carnaval_id_carnaval and carnaval_quesitos.id_carnaval_quesitos = carnaval_quesito_jurado.carnaval_quesitos_id_carnaval_quesitos and carnaval.id_carnaval = carnaval_desfile_escola.carnaval_id_carnaval and carnaval_posicao_jurado.id_carnaval_posicao_jurado = carnaval_quesito_jurado.carnaval_posicao_jurado_id_carnaval_posicao_jurado);";
+		try {
+			pstmt = c.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			while(rs.next()){
+				//desfiles.add(this.resultSet2Object(rs));
+			}
+		} catch (SQLException e) {
+		}finally{
+			closeConnection(c);
 		}
 		return desfiles;
 	}
+	
+	/*private Desfile resultSet2Object(ResultSet rs) throws SQLException
+	{
+		Desfile d = new Desfile();
+		d.setId(rs.getInt("id_integrante"));
+		d.setNome(rs.getString("nome"));
+		d.setEndereco(rs.getString("endereco"));
+		d.setCpf(rs.getString("cpf"));
+		d.setCep(rs.getString("cep"));
+		d.setSexo(Sexos.from(rs.getString("sexo")));
+		d.setEmail(rs.getString("email"));
+		d.setTelefone(rs.getString("telefone"));
+		d.setQuesitoJulgado((Quesito) new QuesitoDAO().obterPorId(rs.getInt("quesito_id_quesito")));
+		return t;
+	}*/
 
 	@Override
 	public Collection<Entidade> obterTodosCollection() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.obterTodos();
 	}
 
 	@Override
